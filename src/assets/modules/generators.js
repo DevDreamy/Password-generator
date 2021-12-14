8d6d004a07efb0213f0ea2c1ebc8b68ea91a15ea
Leo <95947165+DevDreamy@users.noreply.github.com>
2021-12-14 15:29:10 -0300

    Changing const names

--- src/assets/modules/generators.js
+++ src/assets/modules/generators.js
@@ -1,22 +1,27 @@
-const rand = (min, max) => Math.floor(Math.random() * (max-min) + min);
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const generateUppercase = () => String.fromCharCode(rand(65, 91));
 const generateLowercase = () => String.fromCharCode(rand(97, 123));
 const generateNum = () => String.fromCharCode(rand(48, 58));
-const symbols = ',.;~^[]{}!@#$%&*()_+=-';
-const generateSym = () => symbols[rand(0, symbols.length)];
+const specialchar = ',.;~^[]{}!@#$%&*()_+=-';
+const generateSym = () => specialchar[rand(0, specialchar.length)];
 
-export default function generatesPassword(qty, uppercase, lowercase, numbers, symbols) {
+export default function generatesPassword(
+  qty,
+  uppercase,
+  lowercase,
+  numbers,
+  specialchar,
+) {
   const pwArray = [];
   qty = Number(qty);
 
-  for(let i = 0; i<qty; i++) {
+  for (let i = 0; i < qty; i++) {
     uppercase && pwArray.push(generateUppercase());
     lowercase && pwArray.push(generateLowercase());
     numbers && pwArray.push(generateNum());
-    symbols && pwArray.push(generateSym());
+    specialchar && pwArray.push(generateSym());
   }
 
   return pwArray.join('').slice(0, qty);
-
 }
